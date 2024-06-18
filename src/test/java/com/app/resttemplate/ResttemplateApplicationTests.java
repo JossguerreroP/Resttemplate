7e9fd2650efc3ddd1b22b070922e8a8be7d561b9
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-06-18 18:49:31 -0500

    Initial commit

---
+++ src/test/java/com/app/resttemplate/ResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

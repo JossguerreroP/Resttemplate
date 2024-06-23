d60c66de293cfc1c60e5e27a784b2db7d76e231f
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-06-23 09:50:59 -0500

    ok

--- src/main/java/com/app/resttemplate/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/com/app/resttemplate/web/config/BlockingRestTemplateCustomizer.java
@@ -3,6 +3,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.util.Timeout;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,15 +14,30 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnetions;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${sfg.maxtotalconnections}") Integer maxTotalConnections,
+                                          @Value("${sfg.defaultmaxtotalconnections}") Integer defaultMaxTotalConnetions,
+                                          @Value("${sfg.connectionrequesttimeout}")Integer connectionRequestTimeout,
+                                          @Value("${sfg.sockettimeout}")Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnetions = defaultMaxTotalConnetions;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
     public ClientHttpRequestFactory clientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnetions);
 
         var requestConfig = org.apache.hc.client5.http.config.RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(Timeout.ofMilliseconds(3000))
-                .setResponseTimeout(Timeout.ofMilliseconds(3000))
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(defaultMaxTotalConnetions))
+                .setResponseTimeout(Timeout.ofMilliseconds(connectionRequestTimeout))
                 .build();
 
         CloseableHttpClient httpClient = HttpClients

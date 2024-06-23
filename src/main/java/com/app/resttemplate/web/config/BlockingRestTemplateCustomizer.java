1881c88fa170adf3acd7552f26e136ed991c6e81
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-06-23 08:39:18 -0500

    ok

--- src/main/java/com/app/resttemplate/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/com/app/resttemplate/web/config/BlockingRestTemplateCustomizer.java
@@ -1,8 +1,8 @@
 package com.app.resttemplate.web.config;
-
-
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -10,16 +10,33 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
-
-
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-
     public ClientHttpRequestFactory clientHttpRequestFactory() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        var requestConfig = org.apache.hc.client5.http.config.RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(3000))
+                .setResponseTimeout(Timeout.ofMilliseconds(3000))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
         return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
+
+    //    public ClientHttpRequestFactory clientHttpRequestFactory() {
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        return new HttpComponentsClientHttpRequestFactory(httpClient);
+//    }
 
     @Override
     public void customize(RestTemplate restTemplate) {
